9585fb9246a18c99124bb0f6569d1d2d5bb42c7f
Polja137 <117524191+Polja137@users.noreply.github.com>
2023-01-06 15:37:47 +0100

    Added 3 pokemons to array
    Added 3 pokemons to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-alert('Hello world');
-let myName = 'Bob';
-document.write(myName);
+let pokemonList = [];
+
+let bulbasaur = {
+    name: 'bulbasaur',
+    height: 0.7,
+    type: ['grass', 'poison']
+  };
+
+  let pikachu = {
+    name: 'pikachu',
+    height: 0.4,
+    type: ['electric']
+  };
+
+  let clefable = {
+    name: 'clefable',
+    height: 1.3,
+    type: ['fairy']
+  };
+
+  pokemonList[0] = bulbasaur;
+  pokemonList[1]=pikachu;
+  pokemonList[2]=clefable;
+
+
+  
