15e8e99b72d87e9f50ca09ef1cb80e32ed88488d
Polja137 <117524191+Polja137@users.noreply.github.com>
2023-01-13 13:30:21 +0100

    Added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,37 @@
-/*Defined an empty array*/
-let pokemonList = [];
+/*define new variable and wrap in IIFE to eliminate code from global use */
+let pokemonRepository = (function() {
 
-/*Defined new objects and added them to an array*/
+  let pokemonList = [
+    { ID: "1", name: "Bulbasaur", type: ["grass", "poison"], height: 0.7 },
+    { ID: "2", name: "Pikachu", type: ["electic"], height: 0.4},
+    { ID: "3", name: "Clefable", type: ["fairy"], height: 1.3},
+    { ID: "4", name: "Hypno", type: "psychic", height: 1.6},
+    { ID: "5", name: "Jynx", type: ["psychic","ice"], height: 1.4 }]
+   
+  function add(pokemon){
+    pokemonList.push(pokemon);
+  }
 
-let bulbasaur = {
-    name: 'bulbasaur',
-    height: 0.7,
-    type: ['grass', 'poison']
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
   };
 
-  let pikachu = {
-    name: 'pikachu',
-    height: 0.4,
-    type: ['electric']
-  };
-
-  let clefable = {
-    name: 'clefable',
-    height: 1.3,
-    type: ['fairy']
-  };
-
-  pokemonList[0] = bulbasaur;
-  pokemonList[1]=pikachu;
-  pokemonList[2]=clefable;
+}) ();
 
 
 /*replace for loop for forEach loop*/
-pokemonList.forEach (function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.height >=5) {
-      document.write(pokemon.name + " (height: " + pokemon.height + "m) - Wow, that is a big pokemon!" + "<br>")
+      document.write(pokemon.name + " (height: " + pokemon.height + "m) - It's a big pokemon!" + "<br>")
   } else if (pokemon.height >= 1 && pokemon.height < 5) {
-      document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a medium pokemon!" + "<br>")
+      document.write(pokemon.name + " (height: " + pokemon.height + "m) - It's a medium pokemon!" + "<br>")
   } else {
-      document.write(pokemon.name + " (height: " + pokemon.height + "m) - That is a small pokemon!" + "<br>")
+      document.write(pokemon.name + " (height: " + pokemon.height + "m) - It's a small pokemon!" + "<br>")
   }
 //      console.log(pokemon);
   });
